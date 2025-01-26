aa77963536baca485935a733297bbbf2ce3ad2aa
Serge Croisé <SergeCroise@users.noreply.github.com>
2025-01-26 20:34:10 +0100

    comments: fix spelling and grammar in utils.js (#10)
    - a string
    - would also succeed
    - classification

--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,7 @@
  * Checks whether a variable is a string and contains at least one character.
  * 
  * @param {*} string - A variable to check.
- * @returns {boolean} - `true` is the given variable is an string with length > 0, `false` otherwise.
+ * @returns {boolean} - `true` is the given variable is a string with length > 0, `false` otherwise.
  */
 export function hasText(string) {
   return (typeof string === 'string' && string.length > 0);
@@ -33,7 +33,7 @@ export function ensureNumber(num, min, max, delta = 0.00000001) {
 /**
  * Checks whether a variable is a real object or not.
  * 
- * This is a more strict version of `typeof x === 'object'` as this example would also succeeds for arrays and `null`.
+ * This is a more strict version of `typeof x === 'object'` as this example would also succeed for arrays and `null`.
  * This function only returns `true` for real objects and not for arrays, `null` or any other data types.
  * 
  * @param {*} obj - A variable to check.
@@ -122,7 +122,7 @@ export function getMaxForDataType(str) {
 /**
  * Gets the reported minimum and maximum values for a STAC object.
  * 
- * Searches through different extension fields in raster, claasification, and file.
+ * Searches through different extension fields in raster, classification, and file.
  * 
  * @param {StacObject} object 
  * @returns {Statistics}
