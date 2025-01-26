ae5cafac6e7949580ec847eb19fae49f394e33f5
Matthias Mohr <webmaster@mamo-net.de>
2025-01-26 20:53:51 +0100

    Remove special GDAL VFS handling

--- src/http.js
+++ src/http.js
@@ -11,18 +11,6 @@ export const browserProtocols = [
 ];
 
 /**
- * Checks whether a URI is a GDAL Virtual Filesystem URI.
- * 
- * Such an URI usually starts with `/vsi` (except for `/vsicurl/`).
- * 
- * @param {string} href 
- * @returns {boolean} `true` if an GDAL Virtual Filesystem URI, `false` otherwise.
- */
-export function isGdalVfsUri(href) {
-  return typeof href === 'string' && href.startsWith('/vsi') && !href.startsWith('/vsicurl/');
-}
-
-/**
  * 
  * @todo
  * @param {string} href 
@@ -44,13 +32,9 @@ export function toAbsolute(href, baseUrl, stringify = true) {
  * @returns {string|URI}
  */
 export function normalizeUri(href, baseUrl = null, noParams = false, stringify = true) {
-  // Convert vsicurl URLs to normal URLs
-  if (typeof href === 'string' && href.startsWith('/vsicurl/')) {
-    href = href.replace(/^\/vsicurl\//, '');
-  }
   // Parse URL and make absolute, if required
   let uri = URI(href);
-  if (baseUrl && uri.is("relative") && !isGdalVfsUri(href)) { // Don't convert GDAL VFS URIs: https://github.com/radiantearth/stac-browser/issues/116
+  if (baseUrl && uri.is("relative")) { // Don't convert GDAL VFS URIs: https://github.com/radiantearth/stac-browser/issues/116
     // Avoid that baseUrls that have a . in the last parth part will be removed (e.g. https://example.com/api/v1.0 )
     let baseUri = URI(baseUrl);
     let baseUriPath = baseUri.path();
--- tests/http.test.js
+++ tests/http.test.js
@@ -1,24 +1,13 @@
-import { isGdalVfsUri, toAbsolute } from '../src/http';
-
-test('isGdalVfsUri', () => {
-  expect(isGdalVfsUri(null)).toBeFalsy();
-  expect(isGdalVfsUri("")).toBeFalsy();
-  expect(isGdalVfsUri("/vsicurl/example")).toBeFalsy();
-  expect(isGdalVfsUri("http://example.com/vsis3/example")).toBeFalsy();
-
-  expect(isGdalVfsUri("/vsis3/example")).toBeTruthy();
-});
+import { toAbsolute } from '../src/http';
 
 test('toAbsolute', () => {
-  expect(toAbsolute("./example.gif", "https://x.com/catalog.json")).toBe("https://x.com/example.gif");
-  expect(toAbsolute("./", "https://x.com/catalog.json")).toBe("https://x.com/");
-  expect(toAbsolute("/api/example.gif", "https://x.com/api")).toBe("https://x.com/api/example.gif");
-  // expect(toAbsolute("./example.gif", "https://x.com/api")).toBe("https://x.com/api/example.gif");
-  expect(toAbsolute("./example.gif", "https://x.com/api")).toBe("https://x.com/api/example.gif");
-  expect(toAbsolute("./example.gif", "https://x.com/api/")).toBe("https://x.com/api/example.gif");
-  expect(toAbsolute("./example.gif", "https://x.com/catalog.json/")).toBe("https://x.com/catalog.json/example.gif");
-  expect(toAbsolute("http://y.de/test", "https://x.com/catalog.json")).toBe("http://y.de/test");
-  expect(toAbsolute("/vsicurl/http://y.de/test", "https://x.com/catalog.json")).toBe("http://y.de/test");
+  expect(toAbsolute("./example.gif", "https://stac.example/catalog.json")).toBe("https://stac.example/example.gif");
+  expect(toAbsolute("./", "https://stac.example/catalog.json")).toBe("https://stac.example/");
+  expect(toAbsolute("/api/example.gif", "https://stac.example/api")).toBe("https://stac.example/api/example.gif");
+  expect(toAbsolute("./example.gif", "https://stac.example/api")).toBe("https://stac.example/api/example.gif");
+  expect(toAbsolute("./example.gif", "https://stac.example/api/")).toBe("https://stac.example/api/example.gif");
+  expect(toAbsolute("./example.gif", "https://stac.example/catalog.json/")).toBe("https://stac.example/catalog.json/example.gif");
+  expect(toAbsolute("http://y.de/test", "https://stac.example/catalog.json")).toBe("http://y.de/test");
   // todo
 });
 
